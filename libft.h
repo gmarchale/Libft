0fb0e6ece4b07e6d0e2023b671e9c3c36a8f11b8
Gaetan Marchale <gmarchal@fu-r10-p5.s19.be>
2022-10-04 14:24:09 +0200

    all is functions

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 11:33:46 by gmarchal          #+#    #+#             */
+/*   Updated: 2022/10/04 11:40:10 by gmarchal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*memset(void *s, int c, size_t n);
+
+#endif
