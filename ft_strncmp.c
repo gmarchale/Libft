eba9eb1680414422811fef78dd2670f628659d3f
Gaetan Marchale <gmarchal@fu-r2-p7.s19.be>
2022-10-07 15:59:01 +0200

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 18:02:38 by gmarchal          #+#    #+#             */
-/*   Updated: 2022/10/06 11:16:53 by gmarchal         ###   ########.fr       */
+/*   Updated: 2022/10/07 15:58:34 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,20 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	unsigned char	c1;
+	unsigned char	c2;
 
 	i = 0;
-	if (n <= 0)
-		return (0);
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] - s2[i] != 0)
-			return (s1[i] - s2[i]);
+		{
+			c1 = (unsigned char)s1[i];
+			c2 = (unsigned char)s2[i];
+			return (c1 - c2);
+		}
 		i++;
 	}
 	i--;
-	return (s1[i] - s2[i]);
+	return (0);
 }
