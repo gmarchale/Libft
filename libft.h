b5e3492e77fdc5653b8cd3891d6949571bbc28de
Gaetan Marchale <gmarchal@fu-r1-p5.s19.be>
2022-10-10 20:08:42 +0200

    memmove

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 11:33:46 by gmarchal          #+#    #+#             */
-/*   Updated: 2022/10/10 19:32:15 by gmarchal         ###   ########.fr       */
+/*   Updated: 2022/10/10 19:42:47 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,6 @@ char	*ft_strrchr(const char *s, int c);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memove(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
