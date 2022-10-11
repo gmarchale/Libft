b87004104a07919b348d111f8f0fab356ffc6c33
Gaetan Marchale <gmarchal@fu-r8-p7.s19.be>
2022-10-11 14:34:33 +0200

    memchr

--- Makefile
+++ Makefile
@@ -18,6 +18,9 @@ SRCS = ft_strlen.c  \
 	   ft_bzero.c   \
 	   ft_memcpy.c  \
 	   ft_memmove.c \
+	   ft_toupper.c \
+	   ft_tolower.c \
+	   ft_memchr.c  \
 
 OBJ = $(SRCS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 11:33:46 by gmarchal          #+#    #+#             */
-/*   Updated: 2022/10/10 19:42:47 by gmarchal         ###   ########.fr       */
+/*   Updated: 2022/10/11 14:32:39 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,8 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
